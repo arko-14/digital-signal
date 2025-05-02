f9a13e8150073c713e3316b5c2688b0bb1f6ec74
Sandipan Paul <148774271+arko-14@users.noreply.github.com>
2025-05-02 10:23:07 +0530

    Add files via upload

---
+++ butterworthlowpass.m
@@ -0,0 +1,24 @@
+clc;
+close all;
+clear all;
+format long
+rp=input('enter the passband ripple');
+rs=input('enter the stopband ripple');
+wp=input('enter the passband frequency');
+ws=input('enter the stopband frequency');
+fs=input('enter the sampling frequency');
+w1=2*wp/fs;
+w2=2*ws/fs;
+[n,wn]=buttord(w1,w2,rp,rs,'s');
+[z,p,k]=butter(n,wn);
+[b,a]=zp2tf(z,p,k);
+    [b,a]=butter(n,wn,'s');
+    w=0:.01:pi;
+    [h,om]=freqs(b,a,w);
+    m=20*log10(abs(h));
+    an=angle(h);
+    subplot(2,1,1);plot(om/pi,m);
+    ylabel('Gain in DB >');xlabel('(a) Normalised frequency >');
+    subplot(2,1,2);plot(om/pi,an);
+    xlabel('b) Normalised frequency >');
+    ylabel('phase in radians >');
