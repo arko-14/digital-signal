ba4b51f399e60fb1bf2c191f1407bfe1aebf981d
Sandipan Paul <148774271+arko-14@users.noreply.github.com>
2025-05-24 12:12:13 +0530

    Update crosscorr.m

--- crosscorr.m
+++ crosscorr.m
@@ -1,16 +1,30 @@
 clc;
 clear all;
 close all;
-X=input('enter the 1st sequence');
-h=input('enter the 2nd sequence');
-y=Xcorr(x,h);
+
+x = input('Enter the 1st sequence: ');
+h = input('Enter the 2nd sequence: ');
+
+y = xcorr(x, h);  % Cross-correlation
+
 figure;
 subplot(3,1,1);
 stem(x);
 ylabel('Amplitude');
 xlabel('(a)n');
+title('First sequence (x)');
+
 subplot(3,1,2);
 stem(h);
 ylabel('Amplitude');
 xlabel('(b)n');
-disp('The resultant signal is');flep(y)
+title('Second sequence (h)');
+
+subplot(3,1,3);
+stem(y);
+ylabel('Amplitude');
+xlabel('Lag');
+title('Cross-correlation of x and h');
+
+disp('The resultant signal is:');
+disp(y);   % Display the result
